199990ae85127751a0523086ad97677e1161571b
Juan <juannicolash01@gmail.com>
2023-01-02 23:34:49 -0300

    base de header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import Layout from '../components/layout/Layout'
+import { ThemeProvider } from 'styled-components';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />}
+  const theme = {
+    fg: "palevioletred",
+    bg: "white",
+    md: '960px'
+  };
+  return (<>
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  </>)
+}
